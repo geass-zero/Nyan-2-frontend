6bcf32f0f1dc6991dd20685dfc820ff609136e8f
geass-zero <nyanfinance@gmail.com>
2021-01-31 22:24:06 -0500

    Updates

---
+++ precache-manifest.51c725b4317a0910bf4c232360f16cd6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a49839623eb3b1a73f023cf637c39a",
+    "url": "/nyan-2-frontend/index.html"
+  },
+  {
+    "revision": "ad9bd529f9e5c72ff876",
+    "url": "/nyan-2-frontend/static/css/main.b84d9c3b.chunk.css"
+  },
+  {
+    "revision": "bfcbbac07c6c89505e8d",
+    "url": "/nyan-2-frontend/static/js/2.12ae939c.chunk.js"
+  },
+  {
+    "revision": "b547091b077bcebc249210c6d76803fe",
+    "url": "/nyan-2-frontend/static/js/2.12ae939c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9bd529f9e5c72ff876",
+    "url": "/nyan-2-frontend/static/js/main.1ec783c6.chunk.js"
+  },
+  {
+    "revision": "de1cb5b382ef8d013694",
+    "url": "/nyan-2-frontend/static/js/runtime-main.d1a1f977.js"
+  },
+  {
+    "revision": "db3931db6426399b5cb94046e9150cb2",
+    "url": "/nyan-2-frontend/static/media/Dark_Nyan.db3931db.gif"
+  },
+  {
+    "revision": "ddffeeccd93bfa8838233c4c19073785",
+    "url": "/nyan-2-frontend/static/media/Nyan_pump.ddffeecc.gif"
+  },
+  {
+    "revision": "decb4b682f27410f68bed3cfd25567db",
+    "url": "/nyan-2-frontend/static/media/Nyan_stake.decb4b68.gif"
+  },
+  {
+    "revision": "4c656be5e4a8376b161e25bc3555d6d4",
+    "url": "/nyan-2-frontend/static/media/Silkscreen.ttf.4c656be5.eot"
+  },
+  {
+    "revision": "7c04cc3f98eebb5f256d6fc369deea93",
+    "url": "/nyan-2-frontend/static/media/Silkscreen.ttf.7c04cc3f.woff"
+  },
+  {
+    "revision": "a351ea35264d91b1dfdedf6737129ee7",
+    "url": "/nyan-2-frontend/static/media/Silkscreen.ttf.a351ea35.svg"
+  },
+  {
+    "revision": "d6eded6bb9c193b6bda375b08ae4ec4f",
+    "url": "/nyan-2-frontend/static/media/business-cat.d6eded6b.png"
+  },
+  {
+    "revision": "d06a70ea3b2cfa5d8b38ac273715c00b",
+    "url": "/nyan-2-frontend/static/media/cat-logo.d06a70ea.png"
+  },
+  {
+    "revision": "011211f0eb123260d8750de3033d968c",
+    "url": "/nyan-2-frontend/static/media/catnip.011211f0.png"
+  },
+  {
+    "revision": "54609c828ae092e1b1c072984db91a84",
+    "url": "/nyan-2-frontend/static/media/darknyan.54609c82.png"
+  },
+  {
+    "revision": "24fcbc6574978d2fe1b459c6075a0306",
+    "url": "/nyan-2-frontend/static/media/legendary-tier.24fcbc65.jpg"
+  },
+  {
+    "revision": "1704779aef7aa50e50a41a68e371808f",
+    "url": "/nyan-2-frontend/static/media/lock.1704779a.png"
+  },
+  {
+    "revision": "b19924bd2de7a1e6ade08c507d57a08e",
+    "url": "/nyan-2-frontend/static/media/nyan.b19924bd.png"
+  },
+  {
+    "revision": "a823024f323f3cd4345044aa8c51d5ac",
+    "url": "/nyan-2-frontend/static/media/telegram.a823024f.png"
+  }
+]);
